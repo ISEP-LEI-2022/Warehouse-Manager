baa0655023b14b5d643618dc7c30dba95e59905e
Pedro Marques <pedro.ivo996@gmail.com>
2022-11-02 00:02:09 +0000

    Dockerfiles updated. Swagger added.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Warehouse-Manager
 
-Run logistics API directly on Docker Container:
+To Run logistics API directly on Docker Container:
 
-on folder: ~/isep/arqsi/Warehouse-Manager/Sprint_A/code
-cmd: docker-compose -f Docker-compose.yml run --rm --service-ports logistics-api sh -c "npm start"
+on folder: ~/isep/arqsi/Warehouse-Manager/code
+cmd: docker-compose -f Docker-compose.yml run --rm --service-ports logistics-api sh -c "npm run dev"
+
+
+To Run on CMD
+on folder: ~/isep/arqsi/Warehouse-Manager/code/logistics
+cmd: npm run dev
+
+=> To Keep the API documented, on the controllers use decorators @Route(<endpoint>) and @Get("/") [POST/PUT/PATH/DELETE]
--- code/logistics/src/app.ts
+++ code/logistics/src/app.ts
@@ -1,11 +1,26 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+import express, { Application, Request, Response } from "express";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import Router from "./routes";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+const app: Application = express();
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
+app.use(Router);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
 });
