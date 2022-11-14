fa48b1ac3f95f216c5725737f8deb9ab1c22ec6a
nandohos <fernandohenrique2000@gmail.com>
2022-11-14 21:57:39 +0000

    change endpoint style

--- code/logistics/src/application/routes/RouteRouter.ts
+++ code/logistics/src/application/routes/RouteRouter.ts
@@ -24,20 +24,20 @@ router.post("/", async (req: Request, res: Response) => {
   }
 });
 
-router.get("/", async (req: Request, res: Response) => {
+router.get("/:idRoute?", async (req: Request, res: Response) => {
   try {
     const getRouteInstance = <IRouteController>(
       container.get(config.controllers.RouteController.name)
     );
 
-    if (Object.keys(req.query).length === 0) {
-      const listRoutes = await getRouteInstance.getRoutes();
-      res.status(200).json(listRoutes);
-    } else {
+    if (req.params.idRoute !== undefined) {
       const route = await getRouteInstance.getRouteById(
-        req.query.idRoute as string
+        req.params.idRoute as string
       );
       res.status(200).json(route);
+    } else {
+      const listRoutes = await getRouteInstance.getRoutes();
+      res.status(200).json(listRoutes);
     }
   } catch (err) {
     if (err instanceof Err) {
--- code/logistics/src/application/routes/TruckRouter.ts
+++ code/logistics/src/application/routes/TruckRouter.ts
@@ -7,44 +7,45 @@ import { ITruckController } from "../controllers/truck/ITruckController";
 const router = express.Router();
 
 router.post("/", async (req: Request, res: Response) => {
-    try {
-        const createTruckInstance = <ITruckController>(
-        container.get(config.controllers.TruckController.name)
-        );
+  try {
+    const createTruckInstance = <ITruckController>(
+      container.get(config.controllers.TruckController.name)
+    );
 
-        const persisted = await createTruckInstance.createTruck(req.body);
+    const persisted = await createTruckInstance.createTruck(req.body);
 
-        res.status(200).json(persisted);
-    } catch (err) {
-        if (err instanceof Err) {
-        res.status(err.code).send(err.object());
-        }else{
-            res.status(500).send("Unexpected Error" + err);
-        }
+    res.status(200).json(persisted);
+  } catch (err) {
+    if (err instanceof Err) {
+      res.status(err.code).send(err.object());
+    } else {
+      res.status(500).send("Unexpected Error" + err);
     }
-    });
+  }
+});
 
-    router.get("/", async (req: Request, res: Response) => {
-        try {
-        const getTruckInstance = <ITruckController>(
-            container.get(config.controllers.TruckController.name)
-        );
-        if (Object.keys(req.query).length === 0) {
-            const listRoutes = await getTruckInstance.getTrucks();
-            res.status(200).json(listRoutes);
-        } else {
-            const route = await getTruckInstance.getTruckByRegistration(
+router.get("/:registration?", async (req: Request, res: Response) => {
+  try {
+    const getTruckInstance = <ITruckController>(
+      container.get(config.controllers.TruckController.name)
+    );
+
+    if (req.params.registration !== undefined) {
+        const truck = await getTruckInstance.getTruckByRegistration(
             req.query.registration as string
-            );
-            res.status(200).json(route);
-        }
-        } catch (err) {
-        if (err instanceof Err) {
-            res.status(err.code).send(err.object());
-        } else {
-            res.status(500).send("Unexpected Error" + err);
-        }
-        }
-    });
+          );
+          res.status(200).json(truck);
+      } else {
+        const listTrucks = await getTruckInstance.getTrucks();
+        res.status(200).json(listTrucks);
+      }
+    } catch (err) {
+      if (err instanceof Err) {
+        res.status(err.code).send(err.object());
+      } else {
+        res.status(500).send("Unexpected Error" + err);
+      }
+    }
+});
 
 export default router;
