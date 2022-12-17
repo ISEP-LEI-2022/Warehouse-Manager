b4eb2831dfa5cde8967412c3769e2b38832b9bf1
Ana Guedes <1170641@isep.ipp.pt>
2022-12-17 19:17:50 +0000

    update StorageMap

--- code/spa/src/services/mappers/StorageMap.ts
+++ code/spa/src/services/mappers/StorageMap.ts
@@ -11,4 +11,19 @@ export default class StorageMap {
     }
     return storageList;
   }
+
+  public static fromAnyArray(storage: Array<any>): Storage {
+    let obj = Object.assign({}, ...storage.map((x) => ({ [x.name]: x.value })));
+    return new Storage(obj.designation);
+  }
+
+  public static toJson(storage: Storage): string {
+    return JSON.stringify({
+      registration: storage.Designation
+    }, null, "\t");
+  }
+
+  public static empty(): Storage {
+    return new Storage("Default Storage");
+  }
 }
