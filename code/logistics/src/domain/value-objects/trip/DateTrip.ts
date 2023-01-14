025ae2ea0e257d3908abab0c8ee230c4214abf2c
Fernando Santos <nando@DESKTOP>
2023-01-14 15:37:33 +0000

    only filter by date

--- code/logistics/src/domain/value-objects/trip/DateTrip.ts
+++ code/logistics/src/domain/value-objects/trip/DateTrip.ts
@@ -6,9 +6,9 @@ export default class DateTrip {
   
     constructor(dateTrip: Date) {
       const error = businessRuleErrorFactory();
-  
+      const dateWithoutTime = new Date(new Date(dateTrip).toLocaleDateString());
       isValidDate(dateTrip)
-        ? (this.dateTrip = dateTrip)
+        ? (this.dateTrip = dateWithoutTime)
         : error.addError("Invalid date - format needs to be YYYY/MM/DD | YYYY-MM-DD | MM/DD/YYYY | MM-DD-YYYY");
       if (error.hasErrors()) throw error;
     }
