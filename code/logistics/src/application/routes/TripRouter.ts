143ab3f235fa5cefc5bf5d0877a3344f60b118e2
Pedro Marques <pedro.ivo996@gmail.com>
2023-01-16 16:36:38 +0000

    TripRouter url fix.

--- code/logistics/src/application/routes/TripRouter.ts
+++ code/logistics/src/application/routes/TripRouter.ts
@@ -111,7 +111,7 @@ router.put(
         body: JSON.stringify(notOptimized),
       };
 
-      const resp = await fetch("http://env.python_api+"/optimize", options)
+      const resp = await fetch(`http://${env.python_api}/optimize`, options)
         .then((response) => {
           if (response.status === 404) {
             error.addError("not found");
@@ -147,7 +147,7 @@ router.get("/records/pagination/data/", async (req: Request, res: Response) => {
       Number(req.query.page),
       Number(req.query.pageRecords)
     );
-    
+
     res.status(200).json(listTrips);
   } catch (err) {
     if (err instanceof Err) {
